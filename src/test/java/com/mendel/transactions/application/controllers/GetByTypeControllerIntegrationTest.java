901752243b48cac0afc2cd11b62b5d24b63e94ab
Joaquin Ijalba <joaquinijalba@gmail.com>
2023-03-02 21:32:26 -0300

    Create functional Test for GetByTypeController

---
+++ src/test/java/com/mendel/transactions/application/controllers/GetByTypeControllerIntegrationTest.java
@@ -0,0 +1,101 @@
+package com.mendel.transactions.application.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mendel.transactions.domain.entities.Transaction;
+import com.mendel.transactions.domain.entities.TransactionBuilder;
+import com.mendel.transactions.domain.interfaces.IGetByTypeService;
+import com.mendel.transactions.infrastructure.exceptions.ServiceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GetByTypeControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IGetByTypeService getByTypeService;
+
+    private static final String TEST_TYPE = "test";
+    private static final String ERROR_MESSAGE = "Invalid type";
+
+    @Test
+    void testOk() throws Exception {
+        var firstTransaction = new TransactionBuilder()
+                .withId(1L)
+                .withType(TEST_TYPE)
+                .build();
+        var secondTransaction = new TransactionBuilder()
+                .withId(2L)
+                .withType(TEST_TYPE)
+                .build();
+        var thirdTransaction = new TransactionBuilder()
+                .withId(3L)
+                .withType(TEST_TYPE)
+                .build();
+        var transactions = Arrays.asList(
+                firstTransaction,
+                secondTransaction,
+                thirdTransaction
+        );
+
+        when(getByTypeService.execute(TEST_TYPE)).thenReturn(transactions);
+
+        var result = mockMvc.perform(get("/api/transactions/types/" + TEST_TYPE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var expectedResponseBody = transactions.stream()
+                .map(Transaction::getId)
+                .collect(Collectors.toList());
+
+        var responseBody = result.getResponse().getContentAsString();
+        List<Long> ids = new ObjectMapper().readValue(responseBody, new TypeReference<>() {
+        });
+
+        Assertions.assertEquals(expectedResponseBody, ids);
+    }
+
+    @Test
+    void testNotFound() throws Exception {
+        when(getByTypeService.execute(TEST_TYPE)).thenReturn(null);
+
+        MvcResult result = mockMvc.perform(get("/api/transactions/types/" + TEST_TYPE))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        Assertions.assertEquals("No transactions found for type: " + TEST_TYPE, result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testServiceException() throws Exception {
+
+        when(getByTypeService.execute(TEST_TYPE)).thenThrow(new ServiceException(ERROR_MESSAGE));
+
+        // Act
+        MvcResult result = mockMvc.perform(get("/api/transactions/types/" + TEST_TYPE))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        // Assert
+        Assertions.assertEquals(ERROR_MESSAGE, result.getResponse().getContentAsString());
+    }
+}
